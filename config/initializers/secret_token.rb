8110d9610289aabcd67b5d2ae58b5177dfc93fc7
jjunior <jhjunior.pereira@gmail.com>
2011-10-16 01:49:34 -0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AulaApp::Application.config.secret_token = '549fd21eb9e90e73469c2743e674314a0b8df5a71208bb69a98415815dc9f1777d24842806a0d9d3344da8c22001fcb1218d830b72e3e7fec9571458aa809312'
